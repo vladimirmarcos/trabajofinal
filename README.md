ec6204733016ac2e1c00062153e51677da0e50ef
Vladimir Marcos <vegavladimir1992@gmail.com>
2023-05-28 23:21:31 -0300

    index and form template

--- README.md
+++ README.md
@@ -6,5 +6,7 @@ cd app
 virtualenv env
 env\Scripts\activate.bat
 pip install -r requirements.txt
-
+export FLASK_DEBUG=1
+echo $FLASK_DEBUG
+python -m flask run
 ```
--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
+from form import SignupForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/registro',methods=["GET", "POST"])
-def registro():
-    return render_template("registro.html")
+@app.route("/signup/", methods=["GET", "POST"])
+def show_signup_form():
+    form = SignupForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('index'))
+    return render_template("signup_form.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
