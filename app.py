7394a42797778833a3bde2757822fad7fffcd1be
Vladimir Marcos <vegavladimir1992@gmail.com>
2023-05-30 10:59:49 -0300

    flask login

--- app.py
+++ app.py
@@ -1,23 +1,74 @@
 from flask import Flask,render_template,request,redirect,url_for
-from form import SignupForm
+from form import SignupForm,LoginForm
+from flask_login import LoginManager, logout_user, current_user, login_user, login_required
+from models import users,User,get_user
+
+from werkzeug.urls import url_parse
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
+
+
+
+
+
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    for user in users:
+        if user.id == int(user_id):
+            return user
+    return None
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = get_user(form.email.data)
+        if user is not None and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
+            return redirect(next_page)
+    return render_template('login_form.html', form=form)
+
 @app.route("/signup/", methods=["GET", "POST"])
 def show_signup_form():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = SignupForm()
     if form.validate_on_submit():
         name = form.name.data
         email = form.email.data
         password = form.password.data
-        next = request.args.get('next', None)
-        if next:
-            return redirect(next)
-        return redirect(url_for('index'))
+        # Creamos el usuario y lo guardamos
+        user = User(len(users) + 1, name, email, password)
+        users.append(user)
+        # Dejamos al usuario logueado
+        login_user(user, remember=True)
+        next_page = request.args.get('next', None)
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template("signup_form.html", form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
