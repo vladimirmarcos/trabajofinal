6f454dc1e2d1f47dd6804fa027abc8f966b15883
Vladimir Marcos <vegavladimir1992@gmail.com>
2023-05-31 09:07:24 -0300

    flask with db

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask,render_template,request,redirect,url_for
 from form import SignupForm,LoginForm
 from flask_login import LoginManager, logout_user, current_user, login_user, login_required
-from models import users,User,get_user
-
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.urls import url_parse
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Vladimir/Desktop/flask-final/medico.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
+db = SQLAlchemy(app)
 
-
-
-
-
+from models import User
 
 
 
@@ -24,10 +23,7 @@ def index():
 
 @login_manager.user_loader
 def load_user(user_id):
-    for user in users:
-        if user.id == int(user_id):
-            return user
-    return None
+    return User.get_by_id(int(user_id))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -35,7 +31,7 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = get_user(form.email.data)
+        user = User.get_by_email(form.email.data)
         if user is not None and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             next_page = request.args.get('next')
@@ -49,20 +45,26 @@ def show_signup_form():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = SignupForm()
+    error = None
     if form.validate_on_submit():
         name = form.name.data
         email = form.email.data
         password = form.password.data
-        # Creamos el usuario y lo guardamos
-        user = User(len(users) + 1, name, email, password)
-        users.append(user)
-        # Dejamos al usuario logueado
-        login_user(user, remember=True)
-        next_page = request.args.get('next', None)
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return render_template("signup_form.html", form=form)
+        user = User.get_by_email(email)
+        if user is not None:
+            error = f'El email {email} ya está siendo utilizado por otro usuario'
+        else:
+            # Creamos el usuario y lo guardamos
+            user = User(name=name, email=email)
+            user.set_password(password)
+            user.save()
+            # Dejamos al usuario logueado
+            login_user(user, remember=True)
+            next_page = request.args.get('next', None)
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('index')
+            return redirect(next_page)
+    return render_template("signup_form.html", form=form, error=error)
 
 @app.route('/logout')
 def logout():
@@ -70,8 +72,7 @@ def logout():
     return redirect(url_for('index'))
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
 
 
  
