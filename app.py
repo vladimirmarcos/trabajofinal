222c14f1725edb296b4165c5a8bb6a0ded4d63b0
Vladimir Marcos <vegavladimir1992@gmail.com>
2023-06-05 13:57:18 -0300

    mejorando index

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask,render_template,request,redirect,url_for
+import os
 from form import SignupForm,LoginForm
 from flask_login import LoginManager, logout_user, current_user, login_user, login_required
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.urls import url_parse
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Vladimir/Desktop/flask-final/medico.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'medico.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
