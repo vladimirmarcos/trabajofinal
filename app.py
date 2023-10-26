2ee4aa7f1bb15bc96f286aef1a2ac6b8df2acd8c
Vladimir Marcos <vegavladimir1992@gmail.com>
2023-10-26 11:03:27 -0300

    soluciona el flask login

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask,render_template,request,redirect,url_for
 from form import SignupForm,LoginForm
 from flask_login import LoginManager
+import os 
 
+from flask_sqlalchemy import SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'medico.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 login_manager = LoginManager(app)
 
+db = SQLAlchemy(app)
+from models import User
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -32,6 +39,8 @@ def login():
     form = LoginForm()
     return render_template('login_form.html', form=form)
         
-       
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get_by_id(int(user_id))      
        
       
