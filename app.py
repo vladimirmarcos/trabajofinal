cc000ca3bc8cffa63c2e42391f5f28ac450eeea3
Vladimir Marcos <vegavladimir1992@gmail.com>
2023-10-26 11:28:12 -0300

    requirement que soluciono el flask login y los templates que necesita

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask,render_template,request,redirect,url_for
 from form import SignupForm,LoginForm
-from flask_login import LoginManager
+from flask_login import LoginManager,logout_user, current_user, login_user
 import os 
+from werkzeug.urls import url_parse
+from werkzeug.utils import secure_filename
 
 from flask_sqlalchemy import SQLAlchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -17,30 +19,69 @@ from models import User
 def index():
     return render_template("index.html")
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error=None
+    user=None
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+       
+    form = LoginForm()
+    if (form.validate_on_submit()):
+        
+        user = User.get_by_email(form.email.data)
+        if user is not None:
+            clave=user.check_password(form.password.data)
+            if clave:
+                login_user(user, remember=form.remember_me.data)
+                next_page = request.args.get('next')
+                if not next_page or url_parse(next_page).netloc != '':
+                    next_page = url_for('index')
+                return redirect(next_page)
+            else:
+                 error = f'La contraseña no es la indicada D: '
+                 return render_template('login_form.html', form=form,error=error,user=user)
+        else:
+            #print (user.check_passaword(form.password.data)) 
+            userio=form.email.data
+            error = f'El usuario {userio} no esta registrado'
+            return render_template('login_form.html', form=form,error=error,user=user)
+    return render_template('login_form.html', form=form,error=error,user=user)
+
 @app.route("/signup/", methods=["GET", "POST"])
 def show_signup_form():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = SignupForm()
+    error = None
     if form.validate_on_submit():
         name = form.name.data
         email = form.email.data
         password = form.password.data
-        next = request.args.get('next', None)
-        if next:
-            return redirect(next)
-        return redirect(url_for('index'))
-    return render_template("signup_form.html", form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-  
-   
-       
-    form = LoginForm()
-    return render_template('login_form.html', form=form)
+        usuario_profesion=form.usuario_profesion.data
+        user = User.get_by_email(email)
+        if user is not None:
+            error = f'El email {email} ya está siendo utilizado por otro usuario'
+        else:
+            # Creamos el usuario y lo guardamos
+            
+            user = User(name=name, email=email,usuario_profesion=usuario_profesion)
+            user.set_password(password)
+            user.save()
+            login_user(user, remember=True)
+            next_page = request.args.get('next', None)
+            
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('login')
+            return redirect(next_page)
+    return render_template("signup_form.html", form=form, error=error)
         
 @login_manager.user_loader
 def load_user(user_id):
     return User.get_by_id(int(user_id))      
        
       
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
