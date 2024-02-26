92ea2622247e9ec3c3e5b30627a0e224d99c96ef
Vladimir Marcos <vegavladimir1992@gmail.com>
2024-02-26 19:15:05 -0300

    heroku

--- config/dev.py
+++ config/dev.py
@@ -1,6 +1,9 @@
 from .default import *
 import os
+import sqlalchemy
 
 APP_ENV = APP_ENV_DEVELOPMENT
 
 SQLALCHEMY_DATABASE_URI =  'sqlite:///'+os.path.join(basedir, 'medico.db')
+print ("direcion de base datos")
+print(os.path.join(basedir, 'medico.db'))
